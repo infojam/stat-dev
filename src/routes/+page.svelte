a571991d34eddb49e390bcc0c1e979b2d94723c8
infojam <andy@infojam.co.uk>
2023-05-08 19:36:51 +0000

    test update

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -4,10 +4,5 @@ import "carbon-components-svelte/css/white.css";
 import { Accordion, AccordionItem } from "carbon-components-svelte";
 </script>
 
-<Accordion>
-    <AccordionItem title="Section 1" open> Content 1 </AccordionItem>
-    <AccordionItem title="Section 2"> Content 2 </AccordionItem>
-    <AccordionItem title="Section 3"> Content 3 </AccordionItem>
-  </Accordion>
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+<h1>Welcome to STAT</h1>
+
